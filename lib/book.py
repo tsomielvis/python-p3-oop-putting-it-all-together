1b73e481d93714bcd60395801f8814883dd02b05
tsomielvis@gmail.com <tsomielvis@gmail.com>
2024-09-03 15:01:54 +0300

    completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count  # This will use the property setter
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            raise ValueError('page_count must be an integer')
+        self._page_count = value
+
+    def turn_page(self):
+        print('Flipping the page...wow, you read fast!')
+
+book = Book("Book", 25)
+try:
+    book.page_count = '2'  # This will raise an error
+except ValueError as e:
+    print(e)
+
+print(book.page_count)
+book.turn_page()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
-class Shoe:
-    pass
+class Footwear:
+    def __init__(self, brand_name, shoe_size):
+        self.brand_name = brand_name
+        self._shoe_size = shoe_size
+        self.state = "Brand New"
+
+    def get_shoe_size(self):
+        return self._shoe_size
+
+    def set_shoe_size(self, size):
+        if not isinstance(size, int):
+            print("Error: Size must be an integer.")
+        else:
+            self._shoe_size = size
+
+    def repair(self):
+        print("The footwear has been expertly repaired and is ready to wear!")
+
+    shoe_size = property(get_shoe_size, set_shoe_size)
+
+# Example usage
+nike_footwear = Footwear("Nike", 9)
+nike_footwear.shoe_size = '10'  # This will print an error message
+print(nike_footwear.shoe_size)  # This will print the original size (9)
+print(nike_footwear.repair())  # This will print the repair message
